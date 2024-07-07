80b01a60411656efcf29ec9b656c3bc79c424d75
zkkv <zkkv@proton.me>
2024-07-07 09:32:14 +0200

    Add hasStarted state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import Welcome from "./Welcome"
 import Game from "./Game"
 
 export default function App() {
+	const [hasStarted, setHasStarted] = useState(false)
+
+	function startGame() {
+		setHasStarted(true)
+	}
+
 	return (
 		<div>
-			<Welcome />
-			<Game />
+			{hasStarted ? <Game /> : <Welcome startGame={startGame}/>}
 		</div>
 	)
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,9 @@
-export default function Welcome() {
+export default function Welcome(props) {
 	return (
 		<div>
 			<h1>Awesome Trivia</h1>
 			<h2>Test your knowledge in different areas!</h2>
-			<button>Start quiz</button>
+			<button onClick={props.startGame}>Start quiz</button>
 		</div>
 	)
 }
